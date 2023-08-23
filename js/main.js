400d21e91abf95c92bdf2f00ea06d587918b3eb5
Ebbsteer <95626150+Ebbsteer@users.noreply.github.com>
2023-08-23 12:52:26 +0200

    modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -34,37 +34,7 @@
       el.addEventListener('scroll', listener)
     }
   
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-      let position = window.scrollY + 200
-      navbarlinks.forEach(navbarlink => {
-        if (!navbarlink.hash) return
-        let section = select(navbarlink.hash)
-        if (!section) return
-        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-          navbarlink.classList.add('active')
-        } else {
-          navbarlink.classList.remove('active')
-        }
-      })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-  
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-      let elementPos = select(el).offsetTop
-      window.scrollTo({
-        top: elementPos,
-        behavior: 'smooth'
-      })
-    }
-  
+
     /**
      * Back to top button
      */
@@ -82,44 +52,6 @@
     }
   
     /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-      select('body').classList.toggle('mobile-nav-active')
-      this.classList.toggle('bx-menu')
-      this.classList.toggle('bx-x')
-    })
-  
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function(e) {
-      if (select(this.hash)) {
-        e.preventDefault()
-  
-        let body = select('body')
-        if (body.classList.contains('mobile-nav-active')) {
-          body.classList.remove('mobile-nav-active')
-          let navbarToggle = select('.mobile-nav-toggle')
-          navbarToggle.classList.toggle('bx-menu')
-          navbarToggle.classList.toggle('bx-x')
-        }
-        scrollto(this.hash)
-      }
-    }, true)
-  
-    /**
-     * Scroll with ofset on page load with hash links in the url
-     */
-    window.addEventListener('load', () => {
-      if (window.location.hash) {
-        if (select(window.location.hash)) {
-          scrollto(window.location.hash)
-        }
-      }
-    });
-  
-    /**
      * Hero type effect
      */
     const typed = select('.typed')
@@ -134,13 +66,6 @@
         backDelay: 2000
       });
     }
-  
-    /**
-     * Initiate portfolio lightbox 
-     */
-    const portfolioLightbox = GLightbox({
-      selector: '.portfolio-lightbox'
-    });
   
     /**
      * Animation on scroll
