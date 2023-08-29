56bfce061630ad96164a997e52e8e66e3866ce79
Ebbsteer <Ebba.elversson@gmail.com>
2023-08-29 09:59:55 +0200

    new file:   PHP/cart.php 	new file:   PHP/footer.php 	new file:   PHP/header.php 	new file:   PHP/index.php 	new file:   PHP/productPage.php 	modified:   html/index.html

---
+++ PHP/footer.php
@@ -0,0 +1,31 @@
+
+    <!-- Cookie -->
+    <div id="lawmsg" class="alert alert-info alert-dismissible h6 fade show fixed-bottom" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        &nbsp; We use cookies on this website to distinguish you from other users. We use this data to enhance your experience and for targeted advertising. By continuing to use this website you consent to our use of cookies. For more information, please see our
+        <a href="https://info.profilesonly.com" target="_blank">Cookie Policy</a>.
+      </div>
+  
+      <footer> 
+          <div class="d-flex align-items-center justify-content-center">
+            <div class="footer-img"></div>
+  
+            <div class="copyright">
+              &copy; Copyright <strong><span>Whisker Wonders</span></strong>
+            </div>
+          </div>   
+      </footer>
+  
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class='bx bx-up-arrow-alt'></i></a>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+      
+      <!-- JS Files -->
+      <script type="text/javascript" src="js/main.js"></script>
+      <script type="text/javascript" src="js/cart.js"></script>
+  
+    </body>
+  </html>
