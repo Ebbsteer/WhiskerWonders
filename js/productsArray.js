0f69ac041a277a69596e69f8dd84535352a80551
Your Name <jonathan.thj15@gmail.com>
2023-09-01 09:42:28 +0200

    Adding some js and moving array 	new file:   js/productsArray.js 	modified:   js/search.js 	new file:   js/thisproduct.js

---
+++ js/productsArray.js
@@ -0,0 +1,99 @@
+let products = [{
+
+    id: 1,
+    name: "Bounce & Fetch Buddy",
+    link: "../html/thisproduct.html?id=1",
+    image: "../img/products/B&FB.avif",
+    category: "Dog Toys",
+    price: "149.95",
+    quantity: 0
+}, {
+    id: 2,
+    name: "PuzzlePaws Interactive Toy",
+    link: "../html/products/PuzzlePawsInteractiveToy",
+    image: "../img/products/PIT.avif",
+    category: "Dog Toys",
+    price: "199.90",
+    quantity: 0
+}, {
+    id: 3,
+    name: "TastyTidbits Training Treats",
+    link: "../html/products/TastyTidbitsTrainingTreats",
+    image: "../img/products/TTTT.avif",
+    category: "Dog Treats",
+    price: "84.99",
+    quantity: 0
+}, {
+    id: 4,
+    name: "NutriChomp Dental Chews",
+    link: "../html/products/NutriChompDentalChews",
+    image: "../img/products/NDC.avif",
+    category: "Dog Treats",
+    price: "119.90",
+    quantity: 0
+}, {
+    id: 5,
+    name: "FeatherFrenzy Interactive Wand",
+    link: "../html/products/FeatherFrenzyInteractiveWand",
+    image: "../img/products/FIW.avif",
+    category: "Cat Toys",
+    price: "129.95",
+    quantity: 0
+}, {
+    id: 6,
+    name: "Purrfect Pouncing Plaything",
+    link: "../html/products/PurrfectPouncingPlaything",
+    image: "../img/products/PPP.avif",
+    category: "Cat Toys",
+    price: "99.99",
+    quantity: 0
+}, {
+    id: 7,
+    name: "WhiskerWholesome Cat Treats",
+    link: "../html/products/WhiskerWholesomeCatTreats",
+    image: "../img/products/WCT.avif",
+    category: "Cat Treats",
+    price: "64.90",
+    quantity: 0
+}, {
+    id: 8,
+    name: "NutriMews Crunchy Dental Bites",
+    link: "../html/products/NutriMewsCrunchyDentalBites",
+    image: "../img/products/NCDB.avif",
+    category: "Cat Treats",
+    price: "79.99",
+    quantity: 0
+}, {
+    id: 9,
+    name: "Rolling Retreat Hamster Wheel",
+    link: "../html/products/RollingRetreatHamsterWheel",
+    image: "../img/products/RRHB.avif",
+    category: "Hamster Toys",
+    price: "69.90",
+    quantity: 0
+}, {
+    id: 10,
+    name: "MiniMaze Adventure Playset",
+    link: "../html/products/MiniMazeAdventurePlayset",
+    image: "../img/products/MMAD.avif",
+    category: "Hamster Toys",
+    price: "84.90",
+    quantity: 0
+}, {
+    id: 11,
+    name: "TinyTreats Hamster Delights",
+    link: "../html/products/TinyTreatsHamsterDelights",
+    image: "../img/products/TTHD.avif",
+    category: "Hamster Treats",
+    price: "49.99",
+    quantity: 0
+}, {
+    id: 12,
+    name: "NutriNest Hamster Food Mix",
+    link: "../html/products/NutriNestHamsterFoodMix",
+    image: "../img/products/NNHFM.avif",
+    category: "Hamster Treats",
+    price: "79.90",
+    quantity: 0
+}
+];
