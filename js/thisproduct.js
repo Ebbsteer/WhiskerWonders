7e1445ffc808e36f8ca61f30426dd0b50000d323
Your Name <jonathan.thj15@gmail.com>
2023-09-01 10:13:30 +0200

    modified:   js/productsArray.js 	modified:   js/thisproduct.js

--- js/thisproduct.js
+++ js/thisproduct.js
@@ -5,6 +5,10 @@ addEventListener('load', () => {
     const productQuantity = document.getElementById("thisProductQuantity");
 
     const param = (new URLSearchParams(window.location.search)).get('id'); // Assuming 'category' is the query parameter name.
-
     console.log(param);
+    console.log(products.id);
+
+    if (param == products.id) {
+        console.log("Godkänd");
+    }
  });
