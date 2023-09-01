89b40b6acfc96995169d2a7f9c346dd3afd72e44
Your Name <jonathan.thj15@gmail.com>
2023-09-01 10:36:22 +0200

    Adding info to product page 	modified:   html/thisproduct.html 	modified:   js/thisproduct.js

--- js/thisproduct.js
+++ js/thisproduct.js
@@ -1,14 +1,26 @@
+
+const productName = document.getElementById("thisProductName");
+const productImage = document.getElementById("thisProductImage");
+console.log(productImage);
+const productPrice = document.getElementById("thisProductPrice");
+const productQuantity = document.getElementById("thisProductQuantity");    
+
+const param = (new URLSearchParams(window.location.search)).get('id'); // Assuming 'id' is the query parameter name.
+    
 addEventListener('load', () => {
-    const productName = document.getElementById("thisProductName");
-    const productImage = document.getElementById("thisProductImage");
-    const productPrice = document.getElementById("thisProductPrice");
-    const productQuantity = document.getElementById("thisProductQuantity");
 
-    const param = (new URLSearchParams(window.location.search)).get('id'); // Assuming 'category' is the query parameter name.
-    console.log(param);
-    console.log(products.id);
-
-    if (param == products.id) {
-        console.log("Godkänd");
-    }
- });
+    products.forEach(product => {
+        if (param == product.id) {
+            console.log("Godkänd");
+            // Access the properties of the matching product
+            console.log(product.name); // This will print the name of the matching product
+            console.log(productImage); // This will print the image of the matching product
+            console.log(product.price); // This will print the image of the matching product
+            // Assign values to your HTML elements here
+            productName.textContent = product.name;
+            productImage.src = product.image;
+            productPrice.textContent = product.price;
+            productQuantity.textContent = product.quantity;
+        }
+    });
+});
