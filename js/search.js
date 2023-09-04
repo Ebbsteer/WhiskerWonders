08f935276691c4b5f7e4645d8c26c98388bf59cf
Your Name <jonathan.thj15@gmail.com>
2023-09-04 13:02:44 +0200

    Link to search

--- js/search.js
+++ js/search.js
@@ -11,16 +11,18 @@ window.onload = (event) => {
         const search_image = card.querySelector("[data-image]");
         const body = card.querySelector("[data-body]");
         const price = card.querySelector("[data-price]");
+        const link = card.querySelector("[data-link]");
         header.textContent = product.name;
         search_image.src = product.image;
         body.textContent = product.category;
         price.textContent = product.price + " kr";
+        link.href = product.link;
         userCardContainer.append(card);
 
-        cardElements.push({ name: product.name, category: product.category, price: product.price, element: card });
+        cardElements.push({ name: product.name, category: product.category, price: product.price, link: product.link, element: card });
     });
     console.log("Loaded");
-
+    
     searchInput.addEventListener("input", (e) => {
 
         if (searchInput.value == "") {
